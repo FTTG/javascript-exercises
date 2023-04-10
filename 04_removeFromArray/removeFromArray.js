067601d39be7e476f070269c00eaaf77def3638e
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-10 13:04:41 -0600

    Complete exercise 4 of JS practice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+    // Gets array into a separate variable and then goes over the rest of the arguments.
+    let array = arguments[0];
+    for (i = 1; i < arguments.length; i++) {
+        let position = array.findIndex((element) => element === arguments[i]);
+        // After getting the position of the element to search confirm it exist before splicing.
+        if (position != -1) {
+            array.splice(position, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
