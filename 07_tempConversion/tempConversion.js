ae578560de910de7addddeeb4ad318298bb5c6da
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-10 16:12:55 -0600

    Complete exercise 07 for JS practice

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farh) {
+  // Run the formula for F to C convertion and use Math.round *10 then /10 to round to 1 decimal.
+  return Math.round((farh - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celc) {
+  return Math.round((celc * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
