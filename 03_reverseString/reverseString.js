22d741a2de6dccf28ff385309e70b73535cbb662
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-09 22:58:54 -0600

    Complete exercise 03 of JS practice

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (sentence) {
+    let reversed = sentence.split('');
+    reversed = reversed.reverse();
+    return reversed.join('');
+    // return toString(reversed(sentence.split('')))
 };
 
 // Do not edit below this line
