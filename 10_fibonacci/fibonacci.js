2baa4839297638245441450772a3005e945f760a
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-26 11:43:28 -0600

    Complete exercise 10 of JS practice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    let fibo = [1, 1];
+    if (n < 1) return 'OOPS';
+    if (n < 3) return fibo[n - 1];
+    for (let i = 2; i < n; i++) {
+        fibo[i] = fibo[i - 2] + fibo[i - 1];
+    }
+    return fibo[n - 1];
 };
 
 // Do not edit below this line
