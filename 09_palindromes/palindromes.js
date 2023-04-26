ef2b6055b06379798431608a305dabd1fcc5b09d
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-26 11:06:55 -0600

    Complete exercise 9 of JS practice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let alphanum = string.replace(/[^a-z0-9]/gi, '');
+    alphanum = alphanum.toLowerCase();
+    let arrayString = alphanum.split('');
+    let reverseArray = arrayString.reverse();
+    let reverseString = reverseArray.join('');
+    return alphanum == reverseString ? true : false;
 };
 
 // Do not edit below this line
