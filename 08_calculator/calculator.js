3a9f3a49f89af61d9af7ec0708e19d031aa5a26c
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-26 10:37:42 -0600

    Complete exercise 8 of JS practice

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  const sumTotal = array.reduce((total, num) => {
+    return total + num;
+  }, 0);
+  return sumTotal;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  const product = array.reduce((total, current) => {
+    return total * current;
+  }, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let total = 1;
+  for (let i = 1; i <= n; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
