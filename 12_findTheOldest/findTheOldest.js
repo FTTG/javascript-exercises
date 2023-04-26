767276e8432670305929a0f7a9dc8db45f95c4a9
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-26 12:40:21 -0600

    Complete exercise 12 of JS practice

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    let oldestAge = array.reduce((age, person) => {
+        if (!person.yearOfDeath) person.yearOfDeath = ((new Date).getFullYear());
+        if (person.yearOfDeath - person.yearOfBirth > age) {
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+        return age;
+    }, 0);
+    console.log(oldestAge);
+    return array.find(person => {
+        return person.yearOfDeath - person.yearOfBirth == oldestAge;
+    });
 };
 
 // Do not edit below this line
